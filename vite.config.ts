fd8bbaaae2c8d4811dc1c1a80155b4a9a23169c8
DagnisRR <drremesis@gmail.com>
2024-01-23 22:35:19 +0200

    push-notifications T2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import suidPlugin from "@suid/vite-plugin";
 import solidPlugin from "vite-plugin-solid";
+import worker from "firebase-messaging-sw.js?worker";
 
 export default defineConfig({
   plugins: [suidPlugin(), solidPlugin()],
